38e4b9d66988d73f2f6f5c17d9a60f5c044af1be
Fran Zekan <zekan.fran369@gmail.com>
2021-10-18 12:51:35 +0200

    Add javadoc + fix LinkedListIndexedCollection implementaion with get and clear

--- hw01/src/main/java/hr/fer/oprpp1/custom/collections/LinkedListIndexedCollection.java
+++ hw01/src/main/java/hr/fer/oprpp1/custom/collections/LinkedListIndexedCollection.java
@@ -1,48 +1,75 @@
 package hr.fer.oprpp1.custom.collections;
 
+/**
+ * Collection implementation built like a linked list
+ *
+ * @author franzekan
+ * @version 1.0
+ */
 public class LinkedListIndexedCollection extends Collection {
-//    public static void main(String[] args) {
-//        LinkedListIndexedCollection demo = new LinkedListIndexedCollection();
-//
-//        demo.add("Hello");
-//        demo.add("World");
-//        demo.add("12345");
-//
-//        demo.remove(2);
-//
-//        ListNode tmp = demo.first;
-//        while (tmp != null) {
-//            System.out.println(tmp);
-//            tmp = tmp.next;
-//        }
-//    }
-
+    /**
+     * Internal class used to represent a node in a linked list
+     */
     private static class ListNode {
+        /**
+         * Node value
+         */
         private final Object value;
 
+        /**
+         * Reference to previous node
+         */
         private ListNode previous;
+
+        /**
+         * Reference to next node
+         */
         private ListNode next;
 
+        /**
+         * Constructor with all properties as arguments
+         *
+         * @param value    the value
+         * @param previous the previous
+         * @param next     the next
+         */
         public ListNode(Object value, ListNode previous, ListNode next) {
             this.value = value;
             this.previous = previous;
             this.next = next;
         }
 
+        /**
+         * Constructor with value and previous node
+         *
+         * @param value    the value
+         * @param previous the previous
+         */
         public ListNode(Object value, ListNode previous) {
             this(value, previous, null);
         }
 
+        /**
+         * Constructor with only value
+         *
+         * @param value the value
+         */
         public ListNode(Object value) {
             this(value, null, null);
         }
 
-        private String getHashCode(ListNode ln) {
-            if (ln == null) {
+        /**
+         * Helper for easier debugging and representation
+         *
+         * @param node node to get hashCode for
+         * @return hashCode as hex
+         */
+        private String getHashCode(ListNode node) {
+            if (node == null) {
                 return "null";
             }
 
-            return Integer.toHexString(ln.hashCode());
+            return Integer.toHexString(node.hashCode());
         }
 
         @Override
@@ -56,18 +83,43 @@ public class LinkedListIndexedCollection extends Collection {
         }
     }
 
+    /**
+     * Size of list
+     */
     private int size = 0;
+
+    /**
+     * Reference to the first element in the list
+     */
     private ListNode first = null;
+
+    /**
+     * Reference to the last element in the list
+     */
     private ListNode last = null;
 
+    /**
+     * Empty constructor
+     */
     public LinkedListIndexedCollection() {
     }
 
+    /**
+     * Constructor that copies an existing list into <code>this</code>
+     *
+     * @param initCollection the init collection
+     */
     public LinkedListIndexedCollection(Collection initCollection) {
         this.addAll(initCollection);
         this.size = initCollection.size();
     }
 
+    /**
+     * Adds the value to the list
+     *
+     * @param value value to be added
+     * @throws NullPointerException if value is null
+     */
     @Override
     void add(Object value) {
         if (value == null) {
@@ -87,14 +139,24 @@ public class LinkedListIndexedCollection extends Collection {
         this.last = this.last.next;
     }
 
+    /**
+     * Get element by index
+     *
+     * @param index the index
+     * @return element on index
+     * @throws IndexOutOfBoundsException if index is out of range
+     */
     Object get(int index) {
         if (index < 0 || index >= this.size) {
             throw new IndexOutOfBoundsException();
         }
 
-        ListNode tmp = this.first;
-        for (int i = 0; i < index; i++) {
-            tmp = tmp.next;
+        boolean fromStart = this.size / 2 + 1 > index;
+        int for_max = fromStart ? index : this.size - index - 1;
+
+        ListNode tmp = fromStart ? this.first : this.last;
+        for (int i = 0; i < for_max; i++) {
+            tmp = fromStart ? tmp.next : tmp.previous;
         }
 
         return tmp.value;
@@ -110,7 +172,12 @@ public class LinkedListIndexedCollection extends Collection {
     void clear() {
         this.size = 0;
 
-        // todo: not really
+        ListNode tmp = this.last;
+        while (tmp.previous != null) {
+            tmp = tmp.previous;
+            tmp.next = null;
+        }
+
         this.first = null;
         this.last = null;
     }
@@ -130,12 +197,17 @@ public class LinkedListIndexedCollection extends Collection {
         return false;
     }
 
-    // same as get
+    /**
+     * Remove element by index
+     *
+     * @param index the index
+     * @return the boolean
+     * @throws IndexOutOfBoundsException if index is out of range
+     */
     boolean remove(int index) {
         if (index < 0 || index >= this.size) {
             throw new IndexOutOfBoundsException();
         }
-
 
         if (index == 0) {
             this.first = this.first.next;
@@ -149,8 +221,6 @@ public class LinkedListIndexedCollection extends Collection {
         for (int i = 0; i < index; i++) {
             tmp = tmp.next;
         }
-
-        System.out.printf("Remove: %s\n", tmp);
 
         tmp.previous.next = tmp.next;
         this.size--;
@@ -183,8 +253,6 @@ public class LinkedListIndexedCollection extends Collection {
 
     @Override
     Object[] toArray() {
-        int i = 0;
-
         class GetProcessor extends Processor {
             private int i = 0;
             private final Object[] arr;
@@ -209,6 +277,7 @@ public class LinkedListIndexedCollection extends Collection {
         return processor.getArr();
     }
 
+    @Override
     void forEach(Processor processor) {
         ListNode tmp = this.first;
         while (tmp != null) {
