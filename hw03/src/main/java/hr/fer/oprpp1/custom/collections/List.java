4d00d08d2888876c851a07d3c2cbf3c26994b758
Fran Zekan <zekan.fran369@gmail.com>
2021-11-05 15:48:20 +0100

    [HW03] - Missing javadocs

--- hw03/src/main/java/hr/fer/oprpp1/custom/collections/List.java
+++ hw03/src/main/java/hr/fer/oprpp1/custom/collections/List.java
@@ -1,11 +1,40 @@
 package hr.fer.oprpp1.custom.collections;
 
+/**
+ * The interface List.
+ *
+ * @param <T> the type parameter
+ * @author franzekan
+ */
 public interface List<T> extends Collection<T> {
+    /**
+     * Get t.
+     *
+     * @param index the index
+     * @return the t
+     */
     T get(int index);
 
+    /**
+     * Insert.
+     *
+     * @param value    the value
+     * @param position the position
+     */
     void insert(T value, int position);
 
+    /**
+     * Index of int.
+     *
+     * @param value the value
+     * @return the int
+     */
     int indexOf(Object value);
 
+    /**
+     * Remove.
+     *
+     * @param index the index
+     */
     void remove(int index);
 }
--- hw03/src/main/java/hr/fer/oprpp1/custom/collections/Tester.java
+++ hw03/src/main/java/hr/fer/oprpp1/custom/collections/Tester.java
@@ -1,5 +1,17 @@
 package hr.fer.oprpp1.custom.collections;
 
+/**
+ * The interface Tester.
+ *
+ * @param <T> the type parameter
+ * @author franzekan
+ */
 public interface Tester<T> {
+    /**
+     * Test boolean.
+     *
+     * @param obj the obj
+     * @return the boolean
+     */
     boolean test(T obj);
 }
