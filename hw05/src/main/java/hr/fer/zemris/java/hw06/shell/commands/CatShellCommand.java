47c9852f76df9b27c088b1eca890ec0cc4c6c065
Fran Zekan <zekan.fran369@gmail.com>
2021-12-12 01:50:52 +0100

    [HW05] - Implement charsets in cat

--- hw05/src/main/java/hr/fer/zemris/java/hw06/shell/commands/CatShellCommand.java
+++ hw05/src/main/java/hr/fer/zemris/java/hw06/shell/commands/CatShellCommand.java
@@ -5,19 +5,25 @@ import hr.fer.zemris.java.hw06.shell.Environment;
 import hr.fer.zemris.java.hw06.shell.ShellStatus;
 import hr.fer.zemris.java.hw06.shell.commands.shared.FileLoader;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 import java.util.Collections;
 import java.util.List;
 
 public class CatShellCommand implements ShellCommand {
     @Override
     public ShellStatus executeCommand(Environment env, String arguments) {
-        String filePath = ArgumentParser.singleParamParse(arguments);
+        String[] args = ArgumentParser.parse(arguments, 1, 2);
+        String filePath = args[0];
+
+        CharsetDecoder decoder = (args.length == 2 ? Charset.forName(args[1]) : Charset.defaultCharset()).newDecoder();
+
         FileLoader fileLoader = new FileLoader(filePath);
-        FileLoader.FileLoaderResponse res = fileLoader.loadFile(bytes -> {
-            for (byte c : bytes) {
-                System.out.print((char) c);
-            }
-        });
+        FileLoader.FileLoaderResponse res = fileLoader.loadFile(bytes ->
+                env.write(decoder.decode(ByteBuffer.wrap(bytes)).toString())
+        );
+        env.writeln("");
 
         if (res == FileLoader.FileLoaderResponse.NOT_FOUND) {
             env.errorln("File " + filePath + " not found.");
